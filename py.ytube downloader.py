84673a434c9031524966668f3c91f82515e6e824
harini092006 <harinikiruba0912@gmail.com>
2025-07-02 11:47:56 +0530

    Update py.ytube downloader.py

--- py.ytube downloader.py
+++ py.ytube downloader.py
@@ -1,24 +1,22 @@
-Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
-Enter "help" below or click "Help" above for more information.
+
 >>> from pytube import YouTube
-... 
-... def download_youtube_video(url, save_path="."):
-...     try:
-...         # Create YouTube object
-...         yt = YouTube(url)
-... 
-...         print(f"Title: {yt.title}")
-...         print("Downloading the highest resolution stream...")
-... 
-...         # Get the highest resolution stream
-...         stream = yt.streams.get_highest_resolution()
-... 
-...         # Download the video
-...         stream.download(output_path=save_path)
-... 
-...         print(f"Download completed! Video saved to: {save_path}")
-...     except Exception as e:
-...         print(f"An error occurred: {e}")
-... 
-... if _name_ == "_main_":
-...     video_url = input("Enter the YouTube video URL: ")
+ 
+ def download_youtube_video(url, save_path="."):
+     try:
+         yt = YouTube(url)
+ 
+         print(f"Title: {yt.title}")
+         print("Downloading the highest resolution stream...")
+
+      
+         stream = yt.streams.get_highest_resolution()
+ 
+         
+         stream.download(output_path=save_path)
+ 
+         print(f"Download completed! Video saved to: {save_path}")
+    except Exception as e:
+       print(f"An error occurred: {e}")
+
+if _name_ == "_main_":
+    video_url = input("Enter the YouTube video URL: ")
